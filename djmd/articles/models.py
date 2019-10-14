b52f02508c87700acb6f89b7f52a5c833e5761bf
J.V. Zammit <jvzammit@gmail.com>
2019-10-14 15:18:02 +0200

    Step 4: Renders article content as unescaped HTML.

--- djmd/articles/models.py
+++ djmd/articles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from markdownx.utils import markdownify
+
 
 class Article(models.Model):
     slug = models.SlugField()
@@ -12,3 +14,6 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', kwargs={'slug': self.slug})
+
+    def content_html(self):
+        return markdownify(self.content)
